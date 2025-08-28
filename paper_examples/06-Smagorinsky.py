07ab8f3e30be16aad4b22153f3099081ef984e23
Lorenzo Piu <83226613+LorenzoPiu@users.noreply.github.com>
2025-08-28 11:47:07 +0200

    Update 06-Smagorinsky.py

--- paper_examples/06-Smagorinsky.py
+++ paper_examples/06-Smagorinsky.py
@@ -33,17 +33,42 @@ filtered_field = ap.Field3D(f_name, reactive=False)
 
 filtered_field.compute_strain_rate(save_derivatives=False, save_tensor=True)
 
-filtered_field.plot_z_midplane('S_LES', log=True, vmax=2.6, vmin=.5)
-filtered_field.plot_z_midplane('S_DNS', log=True, vmax=2.6, vmin=.5)
+filtered_field.plot_z_midplane('S_DNS', 
+                               log=True, 
+                               vmax=2.6, 
+                               vmin=.5, 
+                               colormap='Blues',
+                               remove_title=True,
+                               cbar_title=r'$\mathcal{S}$',
+                               scale='m'
+                               )
+filtered_field.plot_z_midplane('S_LES', 
+                               log=True, 
+                               vmax=2.6, 
+                               vmin=.5, 
+                               colormap='Blues',
+                               remove_title=True,
+                               # cbar_title=r'$\mathcal{\overline{S }}$',
+                               remove_y=True,
+                               scale='m'
+                               )
 
 filtered_field.DNS_folder_path = DNS_field.folder_path
 filtered_field.compute_residual_dissipation_rate(mode='DNS')
-filtered_field.Cs=0.5
+filtered_field.Cs=0.14
 filtered_field.compute_residual_dissipation_rate(mode='Smag')
 
-filtered_field.plot_z_midplane('Epsilon_r_DNS',  vmin=-200, vmax=200, transparent=False)
-filtered_field.plot_z_midplane('Epsilon_r_Smag', vmin=-200, vmax=200, transparent=False)
+filtered_field.plot_z_midplane('Epsilon_r_DNS',  vmin=-150, vmax=150, transparent=False)
+filtered_field.plot_z_midplane('Epsilon_r_Smag', vmin=-150, vmax=150, transparent=False)
 
-ap.parity_plot(filtered_field.Epsilon_r_DNS.value, filtered_field.Epsilon_r_Smag.value)
+absolute_error = filtered_field.S_DNS.value - filtered_field.S_LES.value
+error_file_name = 'S_AE_id000.dat'
+error_file_path = os.path.join(filtered_field.data_path, error_file_name)
+ap.save_file(absolute_error, error_file_path)
+ap.add_variable('S_AE', 'S_AE_{}.dat')
+# filtered_field.add_variable('S_AE', 'S_AE_id000.dat')
+filtered_field.update(verbose=True)
+
+ap.parity_plot(filtered_field.Epsilon_r_DNS.value, filtered_field.Epsilon_r_Smag.value, density=True)
 
 
