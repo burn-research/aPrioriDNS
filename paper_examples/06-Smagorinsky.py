2b270d0113da0e197ae70fd9cd7faac6e207067e
Lorenzo Piu <83226613+LorenzoPiu@users.noreply.github.com>
2025-09-04 10:37:07 +0200

    Update 06-Smagorinsky.py

--- paper_examples/06-Smagorinsky.py
+++ paper_examples/06-Smagorinsky.py
@@ -12,22 +12,32 @@ import aPrioriDNS as ap
 import os
 
 data_folder = 'DNS_turbulent_transport'
+filter_size = 8
+Cs = 0.16
 
 # Initialize the field, with the option 'reactive' set to false
 DNS_field = ap.Field3D(os.path.join(data_folder, 'DNS_data_cut'), reactive=False)
 
-# DNS_field.cut([0, 0, 0, 0, 472, 472])
-
-DNS_field.compute_velocity_module()
-DNS_field.plot_z_midplane('U_DNS')
+# DNS_field.compute_velocity_module()
+# DNS_field.plot_z_midplane('U_DNS')
 
 DNS_field.compute_strain_rate(save_derivatives=False, save_tensor=True)
 
-DNS_field.plot_z_midplane('S_DNS', log=True)
+f1 = DNS_field.plot_z_midplane('S_DNS', 
+                          log=True, 
+                          vmax=2.6, 
+                          vmin=.5, 
+                          colormap='Blues',
+                          scale='m',
+                          title=r'$\mathcal{S}=2S_{ij}S_{ij}$',
+                          remove_cbar=True,
+                          transparent=False,
+                          )
+
 
 # Filtering field. 
 # The function will return the relative path of the filtered field folder
-f_name = DNS_field.filter(filter_size=8, filter_type='Gauss')
+f_name = DNS_field.filter(filter_size=filter_size, filter_type='Gauss')
 
 filtered_field = ap.Field3D(f_name, reactive=False)
 
@@ -38,37 +48,54 @@ filtered_field.plot_z_midplane('S_DNS',
                                vmax=2.6, 
                                vmin=.5, 
                                colormap='Blues',
-                               remove_title=True,
-                               cbar_title=r'$\mathcal{S}$',
-                               scale='m'
+                               scale='m',
+                               title=r'$\overline{\mathcal{S} }=\overline{2S_{ij}{S}_{ij}}$',
+                               remove_y=True,
+                               remove_cbar=True,
+                               transparent=False,
                                )
 filtered_field.plot_z_midplane('S_LES', 
                                log=True, 
                                vmax=2.6, 
                                vmin=.5, 
                                colormap='Blues',
-                               remove_title=True,
-                               # cbar_title=r'$\mathcal{\overline{S }}$',
+                               scale='m',
+                               title=r'$\bar{\mathcal{S}}=2\overline{S}_{ij}\overline{S}_{ij}$',
                                remove_y=True,
-                               scale='m'
+                               transparent=False,
                                )
 
 filtered_field.DNS_folder_path = DNS_field.folder_path
 filtered_field.compute_residual_dissipation_rate(mode='DNS')
-filtered_field.Cs=0.14
+filtered_field.Cs=Cs
 filtered_field.compute_residual_dissipation_rate(mode='Smag')
 
 filtered_field.plot_z_midplane('Epsilon_r_DNS',  vmin=-150, vmax=150, transparent=False)
 filtered_field.plot_z_midplane('Epsilon_r_Smag', vmin=-150, vmax=150, transparent=False)
 
-absolute_error = filtered_field.S_DNS.value - filtered_field.S_LES.value
+filtered_field.compute_tau_r(mode='DNS')
+filtered_field.compute_tau_r(mode='Smag')
+
+absolute_error = np.abs(filtered_field.TAU_r_DNS.value - filtered_field.TAU_r_Smag.value)
 error_file_name = 'S_AE_id000.dat'
 error_file_path = os.path.join(filtered_field.data_path, error_file_name)
 ap.save_file(absolute_error, error_file_path)
 ap.add_variable('S_AE', 'S_AE_{}.dat')
-# filtered_field.add_variable('S_AE', 'S_AE_id000.dat')
 filtered_field.update(verbose=True)
 
-ap.parity_plot(filtered_field.Epsilon_r_DNS.value, filtered_field.Epsilon_r_Smag.value, density=True)
+filtered_field.plot_z_midplane('S_AE', 
+                               scale='m',
+                               colormap='Reds',
+                               remove_title=True,
+                               )
+
+ap.parity_plot(filtered_field.TAU_r_DNS.value, 
+               filtered_field.TAU_r_Smag.value, 
+               density=True,
+               cmin=1e-8,
+               # limits=[-500, 500],
+               # ticks=[-400, 400],
+               rel_error=None,
+               )
 
 
