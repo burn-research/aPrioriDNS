7d668068a6572767a871c20e9e089132400a9324
Lorenzo Piu <83226613+LorenzoPiu@users.noreply.github.com>
2026-02-10 17:08:24 +0100

    Updated data download and folder check

--- tutorials/ML_closure_reaction_rates.py
+++ tutorials/ML_closure_reaction_rates.py
@@ -10,11 +10,16 @@ import os
 import aPriori as ap
 from aPriori.DNS import Field3D
 
-directory = os.path.join('..', '..','data','Lifted_H2_subdomain') # change this with your path to the data folder
+# Uncomment the following line if you did not download the dataset yet
+# ap.download(dataset='h2_lifted')
+
+directory = os.path.join('.', 'Lifted_H2_subdomain') # change this with your path to the data folder
+
 T_path = os.path.join(directory,'data', 'T_K_id000.dat')
 print(f"\nChecking the path \'{T_path}\' is correct...")
 if not os.path.exists(T_path):
-    raise ValueError("The path '{T_path}' does not exist in your system. Check to have the correct path to your data folder in the code")
+    print(f"The path '{T_path}' does not exist in your system. Downloading the dataset from Github...")
+    ap.download(dataset='h2_lifted')
 
 # Initialize the DNS field
 DNS_field = Field3D(directory)
